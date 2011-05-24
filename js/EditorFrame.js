464f96140809db9c0c51a351767a309b6cd4b632
Jlake Ou <ouzhiwei@gmail.com>
2011-05-24 17:46:22 +0900

    commit again

---
+++ js/EditorFrame.js
@@ -0,0 +1,88 @@
+/**
+ * @class XrEditor.EditorFrame
+ * @extends Ext.panel.TabPanel
+ *
+ * define EditorFrame class
+ *
+ * @EditorFrame
+ * 
+ * @param {Object} config The config object
+ * @author ouzhiwei@gmail.com (Jlake Ou)
+ */
+Ext.define('XrEditor.EditorFrame', {
+	extend: 'Ext.tab.Panel',
+	alias: 'widget.xrEditorFrame',
+	
+	title: 'Editor Frame Panel',
+	//cls: 'editor',
+	autoScroll: true,
+	border: false,
+
+	initComponent: function(){
+		Ext.apply(this, {
+			dockedItems: [this.createToolbar()],
+			height: '100%',
+			items: [{
+				title: 'Untitled',
+				html: 'Untitle document'
+			}]
+		});
+		this.callParent(arguments);
+	},
+
+	/**
+	 * Create the top toolbar
+	 * @private
+	 * @return {Ext.toolbar.Toolbar} toolbar
+	 */
+	createToolbar: function(){
+		var config = {
+			items: [{
+				//scope: this,
+				handler: this.saveOne,
+				text: 'save',
+				iconCls: 'icon-save'
+			}, {
+				//scope: this,
+				handler: this.saveAll,
+				text: 'save all',
+				iconCls: 'icon-saveall'
+			}, '->', {
+				//scope: this,
+				handler: this.showHelp,
+				text: 'Help',
+				iconCls: 'icon-help'
+			}]
+		};
+		return Ext.create('widget.toolbar', config);
+	},
+	/**
+	 * Set the active editor
+	 * @param {Ext.data.Model} rec The record
+	 */
+	setActive: function(editorId) {
+		this.active = editorId;
+		//this.setActiveEditor(editorId);
+	},
+	/**
+	 * Navigate to the active post in a new window
+	 * @private
+	 */
+	saveOne: function() {
+		XrEditor.Util.showMsg('save one');
+	},
+	/**
+	 * Open the post in a new tab
+	 * @private
+	 */
+	saveAll: function() {
+		XrEditor.Util.showMsg('save all');
+	},
+	/**
+	 * Open the post in a new tab
+	 * @private
+	 */
+	showHelp: function() {
+		XrEditor.Util.showMsg('show Help', 'INFO', 'メッセージ');
+	}
+});
---
+++ js/Util.js
@@ -0,0 +1,76 @@
+/**
+ * @class XrEditor.UTIL
+ * @extends Ext.panel.TabPanel
+ *
+ * define UTIL class
+ *
+ * @constructor
+ * 
+ * @param {Object} config The config object
+ * @author ouzhiwei@gmail.com (Jlake Ou)
+ */
+XrEditor.Util = function() {
+	var _loadMask = {};
+	var _msgCt;
+	
+	return {
+		/**
+		 * Loadingマスクの表示
+		 */
+		showMask: function(sMsg, el, sSize, n){
+			sMsg = sMsg || 'Loading';
+			el = el || document.body;
+			n = n || 0;
+			var config = {
+				msg: sMsg
+			};
+			if(sSize == 'BIG') {
+				config.msgCls = 'big-loading-mask'
+			}
+			_loadMask[n] = new Ext.LoadMask(el, config);
+			_loadMask[n].show();
+		},
+		/**
+		 * Loadingマスクの非表示
+		 */
+		hideMask: function(n){
+			n = n || 0;
+			if(_loadMask[n]) {
+				_loadMask[n].hide();
+			}
+		},
+		/**
+		 * メッセージを表示
+		 */
+		showMsg: function(sMsg, sLevel, sTitle, args) {
+			var params = {
+				title: sTitle || 'エラー',
+				msg: sMsg,
+				buttons: Ext.Msg.OK,
+				icon: Ext.MessageBox[sLevel] || Ext.MessageBox.WARNING
+			};
+			Ext.apply(params, args);
+			Ext.Msg.show(params);
+		},
+		/**
+		 * 一時メッセージ
+		 */
+		flashMsg : function(sMsg, sTitle, args){
+			sTitle = sTitle || 'メッセージ';
+			if(!_msgCt){
+				_msgCt = Ext.DomHelper.insertFirst(document.body, {id:'flash-msg'}, true);
+			}
+			_msgCt.alignTo(document, 'c-c');
+			_msgCt.alignTo(document, 't-t');
+			var sHtml = '<div class="msg">'
+					+ '<div class="x-box-tl"><div class="x-box-tr"><div class="x-box-tc"></div></div></div>'
+					+ '<div class="x-box-ml"><div class="x-box-mr"><div class="x-box-mc">'
+					+ '<h3>' + sTitle + '</h3>' + sMsg + '</div></div></div>'
+					+ '<div class="x-box-bl"><div class="x-box-br"><div class="x-box-bc"></div></div></div>'
+					+ '</div>';
+
+			var m = Ext.DomHelper.append(_msgCt, {html:sHtml}, true);
+			m.slideIn('t').pause(1).ghost("t", {remove:true});
+		}
+	};
+}();
